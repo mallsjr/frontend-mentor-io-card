402b50cfc55f2f097182f170e7b3a44a87a00aaa
Michael Alls <michael.alls@fedex.com>
2020-08-04 14:11:15 -0400

    added active state for share mode in mobile view

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.warn('JS file is connected to html');
+function main() {
+  const shareBtn = document.querySelector('#shareBtn');
+  const shareModal = document.querySelector('.social-share');
+  const footer = document.querySelector('.card-footer');
+  const authorInfo = document.querySelector('#author-info');
+  const imageContainer = shareBtn.querySelector('.image-container');
+
+  shareBtn.addEventListener('click', function (e) {
+    if (!footer.classList.contains('share')) {
+      footer.classList.add('share');
+      authorInfo.classList.add('share');
+      shareModal.classList.add('share');
+      imageContainer.classList.add('share');
+    } else {
+      footer.classList.remove('share');
+      authorInfo.classList.remove('share');
+      shareModal.classList.remove('share');
+      imageContainer.classList.remove('share');
+    }
+  });
+}
+
+main();
